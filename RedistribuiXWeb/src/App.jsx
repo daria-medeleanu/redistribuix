547b2fcb1aa48aa405db7a47b68c717605b163a1
ioanaalexia <popaalexia371@gmail.com>
2026-03-05 17:54:14 +0200

    auth page, sideMenu component

--- RedistribuiXWeb/src/App.jsx
+++ RedistribuiXWeb/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AuthPage from './pages/Auth'
+import DashboardPage from './pages/Dashboard'
+import HomePage from './pages/HomePage'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div >
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="min-h-screen bg-[#f9f6f2] text-[#46190c]">
+          {/* <header className="space-y-4">
+            <Navbar />
+            <div className="h-px w-full bg-[#eddccf]" />
+          </header> */}
+
+          <div className="">
+            <Routes>
+              <Route path="/" element={<DashboardPage />} />
+              <Route path="/home" element={<HomePage />} />
+              {/* <Route path="/notes/new" element={<NoteEditorPage />} />
+              <Route path="/notes/:noteId" element={<NoteEditorPage />} /> */}
+              {/* <Route path="/archive" element={<ArchivePage />} /> */}
+              {/* <Route path="/settings" element={<SettingsPage />} /> */}
+              <Route path="/auth" element={<AuthPage />} />
+              {/* <Route path="*" element={<NotFoundPage />} /> */}
+            </Routes>
+          </div>
       </div>
-      <div className="text-3xl font-bold underline">Vite + React</div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
